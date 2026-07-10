dfb5fee66abfa0883311e0730be6f7857d4f3f9e
Dedy Rizaldi <dedyrizaldi03@gmail.com>
2026-07-11 01:21:24 +0700

    U

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,3 @@ use Illuminate\Support\Facades\Hash;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/reset-admin-password', function () {
-    $user = User::find(1);
-
-    $user->password = Hash::make('Admin123');
-    $user->save();
-
-    return 'Password berhasil direset.';
-});
