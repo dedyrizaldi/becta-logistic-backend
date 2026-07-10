3a14f62acec4d9dd55c15559653fd808e821a121
Dedy Rizaldi <dedyrizaldi03@gmail.com>
2026-07-11 01:03:24 +0700

    Create Route Reset Pass

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/reset-admin-password', function () {
+    $user = User::find(1);
+
+    $user->password = Hash::make('Admin123');
+    $user->save();
+
+    return 'Password berhasil direset.';
 });
